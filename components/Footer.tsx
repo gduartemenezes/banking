7f945db3b7a997f4d7e3fd5daf9173a311b85e33
Gustavo Duarte Menezes <gduartemenezes@gmail.com>
2024-08-16 09:29:48 -0300

    feat: add logout handler

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,26 +1,37 @@
 import { logoutAccount } from "@/lib/actions/user.actions";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-const Footer = async ({ user, type = "desktop" }: FooterProps) => {
+const Footer = ({ user, type = "desktop" }: FooterProps) => {
+  const router = useRouter();
   const handleLogout = async () => {
-    await logoutAccount()
-  }
+    const loggedOut = await logoutAccount();
+
+    if (loggedOut) router.push("/sign-in");
+  };
   return (
     <footer className="footer">
       <div className={type === "mobile" ? "footer_name-mobile" : "footer_name"}>
-        <p className="text-xl font-bold text-gray-700">{user.name[0]}</p>
+        <p className="text-xl font-bold text-gray-700">{user?.name[0]}</p>
       </div>
       <div
         className={type === "mobile" ? "footer_email-mobile" : "footer_email"}
       >
         <h1 className="text-14 truncate font-semibold text-gray-700">
-          {user.email}
+          {user?.name}
         </h1>
-        <p className="text-14 truncate font-normal text-gray-600"></p>
+        <p className="text-14 truncate font-normal text-gray-600">
+          {user?.email}
+        </p>
       </div>
       <div className="footer_image">
-        <Image onClick={handleLogout} src={"/icons/logout.svg"} alt={"logout"} />{" "}
+        <Image
+          onClick={handleLogout}
+          src={"/icons/logout.svg"}
+          alt={"logout"}
+          fill
+        />
       </div>
     </footer>
   );
